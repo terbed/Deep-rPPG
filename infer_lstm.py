dee7ddc1938387cdce38520e95cbb9897e3745c9
terbed <terbed@gmail.com>
2020-04-04 13:41:26 +0200

    Infer LSTM without probabilistic output

--- infer_lstm.py
+++ infer_lstm.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 tr = torch
 
 
-def eval_model(models, testloader, oname, out_size, device):
+def eval_model(models, testloader, oname, device):
     total_loss = []
     result = []
     signal = []
