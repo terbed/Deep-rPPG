31292234c59f954dbc2c3d669545789f28ca4251
terbed <terbed@gmail.com>
2020-04-20 18:26:16 +0200

    minor fixes

--- evals/dp190111.py
+++ evals/dp190111.py
@@ -17,7 +17,8 @@ Fs = 20
 pulse_band = [80./60., 250./60.]
 b, a = butter_bandpass(pulse_band[0], pulse_band[1], Fs, order=3)
 
-ref_path = '/media/nas/PUBLIC/0_training_set/pulse/PPGbenchmark_visible_128x128_8UC3_box.hdf5'
+# ref_path = '/media/nas/PUBLIC/0_training_set/pulse/PPGbenchmark_visible_128x128_8UC3_box.hdf5'
+ref_path = '/Volumes/sztaki/DATA/PPGbenchmark_visible_128x128_8UC3_box.hdf5'
 out_path = '../outputs/dp190111-PPGbenchmark_visible.dat'
 
 output = np.loadtxt(out_path)
@@ -25,22 +26,33 @@ print(output.shape)
 
 with h5py.File(ref_path, 'r') as db:
     ref = db['references']
+    print([key for key in ref.keys()])
     ppg_ref = ref['PPGSignal'][:]
+    pr_ref = ref['PulseNumerical'][:]
 
 out_filt = filtfilt(b, a, output)
-plt.figure(figsize=(6, 12))
-plt.plot(ppg_ref, label='ref')
-plt.plot(out_filt, label='out')
+n = len(out_filt)
+t = np.arange(n)/20/60
+print(n, ppg_ref.shape)
+
+plt.figure(figsize=(12, 6))
+plt.specgram(out_filt, NFFT=512, Fs=20*60, noverlap=492, vmin=-32)
+plt.plot(t, pr_ref[:n], label='ref')
+plt.ylim(80, 250)
+plt.ylabel('Pulse [BPM]')
+plt.ylabel('Time [min]')
+plt.colorbar()
+plt.legend()
+
+
+plt.figure(figsize=(12, 6))
+plt.plot(t, ppg_ref[:n], label='ref')
+plt.plot(t, out_filt, label='out')
 plt.grid()
+plt.ylabel('Time [min]')
 plt.legend()
 plt.show()
 
-w = 128
-n = len(out_filt)
-segnum = n // w
 
-pear_list = []
-for i in range(segnum-1):
-    pear_list.append(pearsonr(out_filt[i*w:(i+1)*w], ppg_ref[i*w:(i+1)*w])[0])
-
-print(f'\nPearson correlation coeff: {np.mean(pear_list)}')
+pr = pearsonr(out_filt, ppg_ref[:n])[0]
+print(f'\nPearson correlation coeff: {pr}')
